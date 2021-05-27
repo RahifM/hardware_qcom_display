20d0c9482180476e29cbb8649d3fa1804b40befa
RahifM <faizel326@gmail.com>
2021-05-27 18:46:25 +0530

    Revert "Add TARGET_QCOM_DISPLAY_VARIANT"
    This reverts commit 56aa4306270007642a108667e351167ba8a64cbb.

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-msm8996)
-
 sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core
 
@@ -20,6 +18,4 @@ else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
-endif
-
 endif
